bc8af464676f1fdd9497c636b0d29ce7a06a90fa
nick11roberts <n3robert@ucsd.edu>
2018-11-07 18:21:33 -0800

    About to make final submission.

--- Fashion.py
+++ Fashion.py
@@ -4,6 +4,7 @@ from sklearn.metrics import mutual_info_score
 from torchvision import transforms, utils
 from scipy.stats import chi2_contingency
 from torch.autograd import Variable
+import entropy_estimators as ee
 import matplotlib.pyplot as plt
 import torch.optim as optim
 import torch.nn as nn
@@ -19,7 +20,9 @@ import gzip
 import io
 import os
 
-
+'''
+Based on https://github.com/ashmeet13/FashionMNIST-CNN
+'''
 class Fashion(Dataset):
 	"""Dataset: https://github.com/zalandoresearch/fashion-mnist
 	Args:
@@ -50,7 +53,6 @@ class Fashion(Dataset):
 	raw = "raw"
 	processsed = "processsed"
 
-
 	def __init__(self, root, train=True, transform=True, download=False):
 		super(Fashion, self).__init__()
 		self.root = root
@@ -68,8 +70,6 @@ class Fashion(Dataset):
 		else:
 			test_path = os.path.join(self.root,self.processsed,"testing_set.pt")
 			self.test_images, self.test_labels = torch.load(test_path)
-
-
 
 	'''
 	__getitem__(index) -> Will return the image and label at the specified index
@@ -95,13 +95,11 @@ class Fashion(Dataset):
 
 		return image,label
 
-
 	def __len__(self):
 		if self.train:
 			return(len(self.train_images))
 		else:
 			return(len(self.test_images))
-
 
 	def transform_process(self, image): #Would or would not return a flipped image
 		self.rotate = random.getrandbits(1)
@@ -112,7 +110,6 @@ class Fashion(Dataset):
 	download(root) -> The function will download and save the MNIST images in raw
 	format under the 'raw' folder under the user specified root directory
 	'''
-
 	def download(self, root):
 		raw_path = os.path.join(self.root,self.raw)
 		processsed_path = os.path.join(self.root,self.processsed)
@@ -146,7 +143,6 @@ class Fashion(Dataset):
 	and store them into the a 'processed' folder under user specified root
 	directory.
 	'''
-
 	def process(self, root):
 		raw_path = os.path.join(self.root,self.raw)
 		processsed_path = os.path.join(self.root,self.processsed)
@@ -172,7 +168,6 @@ class Fashion(Dataset):
 		print("Processed data has been stored in your specified root directory under /processsed")
 		print()
 
-
 	def readimg(self, root, file, magic):
 		image = []
 		path = os.path.join(self.root,self.raw,file)
@@ -183,7 +178,6 @@ class Fashion(Dataset):
 				image.append(list(struct.unpack('B',f.read(1)))[0])
 			image = np.asarray(image, dtype = np.float32)
 			return (torch.from_numpy(image).view(size,1,row,col))
-
 
 	def readlab(self, root, file, magic):
 		label = []
@@ -196,16 +190,13 @@ class Fashion(Dataset):
 			label = np.asarray(label)
 			return (torch.from_numpy(label))
 
-
 train_dataset = Fashion(root = "./FashionMNIST", train = True, transform = True, download = True)
 test_dataset = Fashion(root = "./FashionMNIST", train = False, transform = True, download = True)
-
 
 '''
 Calculation of total epochs using a defined batch size(batch_size)
 and total iterations(n_ters)
 '''
-
 batch_size = 100
 n_iters = 18000
 m_bins = 60
@@ -215,7 +206,6 @@ epoch_size = int(math.ceil(epoch_size))
 '''
 Loading the test dataset (test_loader)
 '''
-
 test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
 
 image_dict = {0:'T-shirt/Top', 1:'Trouser', 2:'Pullover', 3:'Dress',
@@ -224,9 +214,6 @@ image_dict = {0:'T-shirt/Top', 1:'Trouser', 2:'Pullover', 3:'Dress',
 for index, (images,labels) in enumerate(test_loader):
 	image=images[index][0]
 	label = labels[index]
-	#plt.imshow(image)
-	#plt.suptitle(image_dict[label.item()]+" - "+str(label.item()))
-	#plt.show()
 	if index == 5:
 		break
 
@@ -289,22 +276,26 @@ class CNNModel(nn.Module):
 		return out
 
 	def forward_activ(self, x):
+		'''
+		Compute the forward pass and copy the activations for use in the
+		entropy and mutual information estimates
+		'''
 
 		out = self.cnn1(x)
 		out = self.relu1(out)
-		out = self.norm1(out)
-		out = self.maxpool1(out)
 		t1 = out.clone()
 		t1 = t1.cpu().detach().numpy()
 		t1 = t1.reshape(t1.shape[0], t1.shape[1] * t1.shape[2] * t1.shape[3])
+		out = self.norm1(out)
+		out = self.maxpool1(out)
 
 		out = self.cnn2(out)
 		out = self.relu2(out)
-		out = self.norm2(out)
-		out = self.maxpool2(out)
 		t2 = out.clone()
 		t2 = t2.cpu().detach().numpy()
 		t2 = t2.reshape(t2.shape[0], t2.shape[1] * t2.shape[2] * t2.shape[3])
+		out = self.norm2(out)
+		out = self.maxpool2(out)
 
 		out = out.view(out.size(0),-1)
 		out = self.fc1(out)
@@ -319,12 +310,10 @@ class CNNModel(nn.Module):
 
 		return out, t1, t2, t3, t4
 
-def mutual_info(P):
-
-	#print(P)
+def empirical_mutual_info(counts):
 
 	# Normalize the counts
-	P /= np.sum(P)
+	P /= np.sum(counts)
 
 	# Compute marginal probabilities for t and y
 	Pt = np.sum(P, 0)
@@ -342,7 +331,6 @@ def mutual_info(P):
 '''
 Instantiating the model class
 '''
-
 model = CNNModel()
 if torch.cuda.is_available():
 	model.cuda()
@@ -354,12 +342,10 @@ Optimizer: Stochastic Gradient Descent (Nesterov Momentum is enabled).
 Variable: learning_rate -> Stores the learning rate for the optimizer function.
           moment -> Stores the momentum for the optimizer function.
 '''
-
 criterion = nn.CrossEntropyLoss()
 
 learning_rate = 0.015
 moment = 0.9
-#optimizer = optim.SGD(model.parameters(),lr = learning_rate, momentum = moment, nesterov = True)
 optimizer = optim.Adam(model.parameters())
 
 iXT1 = []
@@ -382,17 +368,24 @@ for epoch in range(epoch_size):
 	shuffling of above exampled Fashion MNIST images.
 	'''
 
-	# Empirical layer activation histograms
+	xs = []
+	ys = []
+	t1s = []
+	t2s = []
+	t3s = []
+	t4s = []
+
 	t1_hist = np.zeros(m_bins)
 	t2_hist = np.zeros(m_bins)
 	t3_hist = np.zeros(m_bins)
 	t4_hist = np.zeros(m_bins)
 
-	# Empirical activations vs labels histograms
-	cont_mat_yt1 = np.zeros([10, m_bins])
-	cont_mat_yt2 = np.zeros([10, m_bins])
-	cont_mat_yt3 = np.zeros([10, m_bins])
-	cont_mat_yt4 = np.zeros([10, m_bins])
+	t1_ent = 0
+	t2_ent = 0
+	t3_ent = 0
+	t4_ent = 0
+
+	h_cnt = 0
 
 	train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
 	for i, (images, labels) in enumerate(train_loader):
@@ -404,8 +397,14 @@ for epoch in range(epoch_size):
 			images = Variable(images)
 			labels = Variable(labels)
 
+		X = images.clone()
+		X = X.cpu().detach().numpy()
+		X = X.reshape(X.shape[0], X.shape[2] * X.shape[3]).tolist()
+		xs.extend(X)
+
 		Y = labels.clone()
-		Y = Y.cpu().detach().numpy()
+		Y = Y.cpu().detach().numpy().tolist()
+		ys.extend(Y)
 
 		optimizer.zero_grad()
 		outputs, t1, t2, t3, t4 = model.forward_activ(images)
@@ -414,9 +413,9 @@ for epoch in range(epoch_size):
 
 			# Compute the binned activations from each layer
 			# and keep a running total for each of the layers for this epoch
-			t1_hist_cur = np.histogram(t1[batch_index], m_bins, [0.0, 3.0])[0]
-			t2_hist_cur = np.histogram(t2[batch_index], m_bins, [0.0, 3.0])[0]
-			t3_hist_cur = np.histogram(t3[batch_index], m_bins, [0.0, 12.0])[0]
+			t1_hist_cur = np.histogram(t1[batch_index], m_bins, [0.0, 10.0])[0]
+			t2_hist_cur = np.histogram(t2[batch_index], m_bins, [0.0, 10.0])[0]
+			t3_hist_cur = np.histogram(t3[batch_index], m_bins, [0.0, 10.0])[0]
 			t4_hist_cur = np.histogram(t4[batch_index], m_bins, [0.0, 1.0])[0]
 
 			t1_hist += t1_hist_cur
@@ -424,25 +423,29 @@ for epoch in range(epoch_size):
 			t3_hist += t3_hist_cur
 			t4_hist += t4_hist_cur
 
-			# Update the layer activation histogram counts
-			# per label in the minibatch
-			cont_mat_yt1[Y[batch_index]] += t1_hist_cur
-			cont_mat_yt2[Y[batch_index]] += t2_hist_cur
-			cont_mat_yt3[Y[batch_index]] += t3_hist_cur
-			cont_mat_yt4[Y[batch_index]] += t4_hist_cur
+			t1_ent += scipy.stats.entropy(t1[batch_index])
+			t2_ent += scipy.stats.entropy(t2[batch_index])
+			t3_ent += scipy.stats.entropy(t3[batch_index])
+			t4_ent += scipy.stats.entropy(t4[batch_index])
+			h_cnt += 1
+
+			t1s.extend(t1_hist_cur.tolist())
+			t2s.extend(t2_hist_cur.tolist())
+			t3s.extend(t3_hist_cur.tolist())
+			t4s.extend(t4_hist_cur.tolist())
 
 		loss = criterion(outputs,labels)
 		loss.backward()
 		optimizer.step()
 		iter += 1
 
-
 		'''
-		At every 3000th epoch a test on the above initialised test dataset
+		At every 30th minibatch, a test on the above initialised test dataset
 		(test_loader) would be performed and an accuracy would be provided.
+		In addition, the average entropies and mutual information scores will
+		be compiled and cached to disk.
 		'''
-
-		if iter%30 == 0: #iter%3000 == 0:
+		if iter%30 == 0:
 			correct = 0
 			total = 0
 			for image,label in test_loader:
@@ -467,32 +470,50 @@ for epoch in range(epoch_size):
 			# I(X;T) = I(T;X)
 			#        = H(T) - H(T|X)
 			#        = H(T)
-			iXT1.append(scipy.stats.entropy(t1_hist))
-			iXT2.append(scipy.stats.entropy(t2_hist))
-			iXT3.append(scipy.stats.entropy(t3_hist))
-			iXT4.append(scipy.stats.entropy(t4_hist))
+			# In this case, I use the average entropy
+			# over the training examples
+			iXT1.append(t1_ent / h_cnt)
+			print(iXT1)
+			iXT2.append(t2_ent / h_cnt)
+			print(iXT2)
+			iXT3.append(t3_ent / h_cnt)
+			print(iXT3)
+			iXT4.append(t4_ent / h_cnt)
+			print(iXT4)
 
-			iTY1.append(mutual_info(cont_mat_yt1))
-			iTY2.append(mutual_info(cont_mat_yt2))
-			iTY3.append(mutual_info(cont_mat_yt3))
-			iTY4.append(mutual_info(cont_mat_yt4))
+			# Estimate the continuous mutual information using the
+			# k-nearst neighbors estimator
+			# https://github.com/gregversteeg/NPEET
+			ys = ee.vectorize(ys)
+			iTY1.append(ee.mi(t1s, ys))
+			print(iTY1)
+			iTY2.append(ee.mi(t2s, ys))
+			print(iTY2)
+			iTY3.append(ee.mi(t3s, ys))
+			print(iTY3)
+			iTY4.append(ee.mi(t4s, ys))
+			print(iTY4)
 
-			#t1_hist = np.zeros(m_bins)
-			#t2_hist = np.zeros(m_bins)
-			#t3_hist = np.zeros(m_bins)
-			#t4_hist = np.zeros(m_bins)
+			xs = []
+			ys = []
+			t1s = []
+			t2s = []
+			t3s = []
+			t4s = []
 
-			#cont_mat_yt1 = np.zeros([10, m_bins])
-			#cont_mat_yt2 = np.zeros([10, m_bins])
-			#cont_mat_yt3 = np.zeros([10, m_bins])
-			#cont_mat_yt4 = np.zeros([10, m_bins])
+			t1_ent = 0
+			t2_ent = 0
+			t3_ent = 0
+			t4_ent = 0
 
-np.array(iXT1).tofile('iXT1.dat')
-np.array(iXT2).tofile('iXT2.dat')
-np.array(iXT3).tofile('iXT3.dat')
-np.array(iXT4).tofile('iXT4.dat')
+			h_cnt = 0
 
-np.array(iTY1).tofile('iTY1.dat')
-np.array(iTY2).tofile('iTY2.dat')
-np.array(iTY3).tofile('iTY3.dat')
-np.array(iTY4).tofile('iTY4.dat')
+			np.array(iXT1).tofile('iXT1.dat')
+			np.array(iXT2).tofile('iXT2.dat')
+			np.array(iXT3).tofile('iXT3.dat')
+			np.array(iXT4).tofile('iXT4.dat')
+
+			np.array(iTY1).tofile('iTY1.dat')
+			np.array(iTY2).tofile('iTY2.dat')
+			np.array(iTY3).tofile('iTY3.dat')
+			np.array(iTY4).tofile('iTY4.dat')
--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # FashionMNIST
-My attempt in implementing a network for the dataset Fashion MNIST which can be found at:
-    https://github.com/zalandoresearch/fashion-mnist
-    
-My model:
 
-    - 2 Layered CNN + Max Pooling + Batch Normalisation (Using ReLU activation)
+CNN based on the architecture and implementation by ashmeet13 [1]
+to attain high accuracy on Fashion MNIST [2]
+and subsequently visualize the information plane [3]
+
+This model is comprised of the following layers:
+    - 2 conv layers + Max Pooling + Batch Normalisation (Using ReLU activation)
     - 2 Fully Connected Layers
-    
-    Random Horizontal Flips of the Fashion MNIST images is included as preprocessing technique.
-    
-    Optimizer Used - SGD + Nesterov Momentum
-    
+
+    Random Horizontal Flips of the Fashion MNIST images is included as a data augmentation technique.
+
+    Optimizer Used - Adam
+
 Final Accuracy - 93.29%
+
+Notes on the information plane visualization:
+    - Mutual information between the labels and the hidden layers were originally computed empirically, but [4], which uses a k nearest neighbors estimator, yielded slightly more reasonable mutual information estimates
+    - To visualize the information plane, follow the instructions below
+
+Visualizing the information plane
+Run `Fashion.py` to generate the appropriate `.dat` files containing the cached mutual information scores (updated every epoch (or more precisely, every 300 minibatches, because the term epoch loses some meaning when random data augmentation is used))
+```
+python3 Fashion.py
+```
+then run the cells in `information_plane.ipynb` to generate the plot.
+
+References
+[1] https://github.com/ashmeet13/FashionMNIST-CNN
+[2] https://github.com/zalandoresearch/fashion-mnist
+[3] https://arxiv.org/abs/1703.00810
+[4] https://github.com/gregversteeg/NPEET
